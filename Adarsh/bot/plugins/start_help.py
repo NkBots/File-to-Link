e4c2681d9b717a9b3d4e0074baf4a4c3351e7982
NkBots <116135526+NkBots@users.noreply.github.com>
2022-11-24 14:16:18 +0530

    Add files via upload

---
+++ Adarsh/bot/plugins/start_help.py
@@ -0,0 +1,270 @@
+#Aadhi000 
+from Adarsh.bot import StreamBot
+from Adarsh.vars import Var
+import logging
+logger = logging.getLogger(__name__)
+from Adarsh.bot.plugins.stream import MY_PASS
+from Adarsh.utils.human_readable import humanbytes
+from Adarsh.utils.database import Database
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant
+from Adarsh.utils.file_properties import get_name, get_hash, get_media_file_size
+db = Database(Var.DATABASE_URL, Var.name)
+from pyrogram.types import ReplyKeyboardMarkup
+
+                      
+
+buttonz=ReplyKeyboardMarkup(
+            [
+                ["start","help"],
+                ["about","ping"],
+                ["status"]        
+            ],
+            resize_keyboard=True
+        )
+
+START_TEXT = """
+Hey {} 👋\n
+<code>I am Telegram File To Link Bot</code>\n
+<code>Use Help Command to Know how to Use me</code>\n
+**@Tellybots**"""
+
+HELP_TEXT = """
+✮ Send Me Any File or Media\n
+✮ I Will Provide You Instant Direct Download link as Well as Stream Link\n
+✮ Add me in Your Channel as Admin To Get Direct Download link button and online Stream Link Button\n
+"""
+
+
+ABOUT_TEXT = """
+🤖 My Name : Telly File Stream Bot\n
+🚦 Version : <a href='https://telegram.me/tellybots'>3.0</a>\n
+💫 Source Code : <a href='https://t.me/tellybots_digital'>Click Here</a>\n
+🗃️ Library : <a href='https://pyrogram.org'>Click Here</a>\n
+👲 Developer : <a href='https://telegram.me/tellybots'>TellyBots</a>\n
+📦 Last Updated : <a href='https://telegram.me/tellybots'>[ 13-Jan-22 ] 09:00 AM</a>"""
+
+TEXT = """Just Send Me Any Telegram Media To Get Started\n\nOr Use Below Buttons to Interact With Me"""
+
+
+INFO_TEXT = """
+ 💫 Telegram Information
+
+ 🤹 First Name : <b>{}</b>
+
+ 🚴‍♂️ Second Name : <b>{}</b>
+
+ 🧑🏻‍🎓 Username : <b>@{}</b>
+
+ 🆔 Telegram Id : <code>{}</code>
+
+ 📇 Profile Link : <b>{}</b>
+
+ 📡 Dc : <b>{}</b>
+
+ 📑 Language : <b>{}</b>
+
+ 👲 Status : <b>{}</b>
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🌷 Help', callback_data='help'),
+        InlineKeyboardButton('💠 About', callback_data='about')
+        ],[
+        InlineKeyboardButton('📛 Close', callback_data='close')
+        ]]
+)
+             
+
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('💠 About', callback_data='about')
+        ],[
+        InlineKeyboardButton('📛 Close', callback_data='close')
+        ]]
+)
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('🌷 Help', callback_data='help')
+        ],[
+        InlineKeyboardButton('📛 Close', callback_data='close')
+        ]]
+)
+ 
+
+@StreamBot.on_message(filters.command('start') & filters.private)
+async def start(b, m):
+    if not await db.is_user_exist(m.from_user.id):
+        await db.add_user(m.from_user.id)
+        await b.send_message(
+            Var.BIN_CHANNEL,
+            f"#NEW_USER: \n\nNew User [{m.from_user.first_name}](tg://user?id={m.from_user.id}) Started !!"
+        )
+    usr_cmd = m.text.split("_")[-1]
+    if usr_cmd == "/start":
+        if Var.UPDATES_CHANNEL is not None:
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "banned":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="**ʏᴏᴜ ᴀʀᴇ ʙᴀɴɴᴇᴅ../**",
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ  ᴍᴇ..**\n\n**ᴅᴜᴇ ᴛᴏ ᴏᴠᴇʀʟᴏᴀᴅ ᴏɴʟʏ ᴄʜᴀɴɴᴇʟ sᴜʙsᴄʀɪʙᴇʀs ᴄᴀɴ ᴜsᴇ ᴍᴇ..!**",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("𝙹𝙾𝙸𝙽 𝚄𝙿𝙳𝙰𝚃𝙴𝚉 𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ]
+                        ]
+                    )
+                    
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Please Join My Update Channel or Try Again **",
+                    
+                    disable_web_page_preview=True)
+                return
+        await m.reply_text(
+            
+            text=START_TEXT.format(m.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+            
+        )
+    else:
+        if Var.UPDATES_CHANNEL is not None:
+            try:
+                user = await b.get_chat_member(Var.UPDATES_CHANNEL, m.chat.id)
+                if user.status == "banned":
+                    await b.send_message(
+                        chat_id=m.chat.id,
+                        text="**You are Banned**",
+                        
+                        disable_web_page_preview=True
+                    )
+                    return
+            except UserNotParticipant:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**Join My Update Channel to Use me.**\n\n**Due to Overload Only Channel Subscribers can use me ..!**",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("ᴊᴏɪɴ ᴍʏ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{Var.UPDATES_CHANNEL}")
+                            ]                           
+                        ]
+                    )
+                    
+                )
+                return
+            except Exception:
+                await b.send_message(
+                    chat_id=m.chat.id,
+                    text="**𝙰𝙳𝙳 𝙵𝙾𝚁𝙲𝙴 𝚂𝚄𝙱 𝚃𝙾 𝙰𝙽𝚈 𝙲𝙷𝙰𝙽𝙽𝙴𝙻**",
+                    disable_web_page_preview=True)
+                return
+
+        get_msg = await b.get_messages(chat_id=Var.BIN_CHANNEL, ids=int(usr_cmd))
+        msg_text = "Your Link Generated 📩\n\n🗄️ File Name : <code>{}</code>\n\n📇 File Size : <code>{}</code>\n\n📥 Download Link : <code>{}</code>\n\n🎥 Watch Online : <code>{}</code>"
+
+        file_size = None
+        if get_msg.video:
+            file_size = f"{humanbytes(get_msg.video.file_size)}"
+        elif get_msg.document:
+            file_size = f"{humanbytes(get_msg.document.file_size)}"
+        elif get_msg.audio:
+            file_size = f"{humanbytes(get_msg.audio.file_size)}"
+
+        file_name = None
+        if get_msg.video:
+            file_name = f"{get_msg.video.file_name}"
+        elif get_msg.document:
+            file_name = f"{get_msg.document.file_name}"
+        elif get_msg.audio:
+            file_name = f"{get_msg.audio.file_name}"
+
+        stream_link = "https://{}/{}".format(Var.FQDN, get_msg.id) if Var.ON_HEROKU or Var.NO_PORT else \
+            "http://{}:{}/{}".format(Var.FQDN,
+                                     Var.PORT,
+                                     get_msg.id)
+
+        
+        await m.reply_photo(
+            photo="https://telegra.ph/file/565f1e1b578ed4e682b7f.jpg",
+            caption=msg_text.format(file_name, file_size, stream_link),
+            
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📩 Download link", url=stream_link)]])
+        )
+
+
+
+        
+
+@StreamBot.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    else:
+        await update.message.delete()
+ 
+ #Recoded By Tellybots
+
+@StreamBot.on_message((filters.command("about")) & filters.private)
+async def about(bot, update):
+
+    await update.reply_text(
+        text=ABOUT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=ABOUT_BUTTONS
+    ) 
+@StreamBot.on_message((filters.command("help")) & filters.private)
+async def help(bot, update):
+
+    await update.reply_text(
+        text=HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=HELP_BUTTONS
+    )
+
+@StreamBot.on_message(filters.private & filters.command("info"))
+async def info_handler(bot, update):
+
+
+    if update.from_user.last_name:
+        last_name = update.from_user.last_name
+    else:
+        last_name = "None"
+
+  
+    await update.reply_text(  
+        text=INFO_TEXT.format(update.from_user.first_name, last_name, update.from_user.username, update.from_user.id, update.from_user.mention, update.from_user.dc_id, update.from_user.language_code, update.from_user.status),             
+        disable_web_page_preview=True
+    )
