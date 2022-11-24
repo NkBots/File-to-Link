79313885ab22d1ab02c0bf221f2a4695f4c1a8df
NkBots <116135526+NkBots@users.noreply.github.com>
2022-11-24 22:39:47 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -30,11 +30,11 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.onrender.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
-    if HAS_SSL:
-        URL = "https://nkfiletolink.onrender.com/"
-    else:
-        URL = "http://nkfiletolink.onrender.com/"
+    #HAS_SSL=bool(getenv('HAS_SSL',False))
+    #if HAS_SSL:
+        #URL = "https://nkfiletolink.onrender.com/"
+   # else:
+        #URL = "http://nkfiletolink.onrender.com/"
     DATABASE_URL = str(getenv('DATABASE_URL'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
