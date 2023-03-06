caa215c7eabd12e4f591f9bc623d856d75441804
NkBots <116135526+NkBots@users.noreply.github.com>
2023-03-06 16:13:37 +0530

    Update __init__.py

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -10,18 +10,13 @@ from pyrogram import Client
 
 logger = logging.getLogger("bot")
 
-sessions_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "sessions")
-if Var.USE_SESSION_FILE:
-    logger.info("Using session files")
-    logger.info("Session folder path: {}".format(sessions_dir))
-    if not os.path.isdir(sessions_dir):
-        os.makedirs(sessions_dir)
+
 
 StreamBot = Client(
     name="WebStreamer",
     api_id=Var.API_ID,
     api_hash=Var.API_HASH,
-    workdir=sessions_dir if Var.USE_SESSION_FILE else "WebStreamer",
+    
     plugins={"root": "Adarsh/bot/plugins"},
     bot_token=Var.BOT_TOKEN,
     sleep_threshold=Var.SLEEP_THRESHOLD,
